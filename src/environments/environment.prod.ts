ab58742cada2d1c082e0f0a306c2b2f9852e502b
= <=>
2022-01-31 08:10:53 -0500

    creating build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAjKmKhIa1IFCaXBINmUlrGN_RHnDXsBJ4",
+    authDomain: "ng-mitienda-piloto.firebaseapp.com",
+    projectId: "ng-mitienda-piloto",
+    storageBucket: "ng-mitienda-piloto.appspot.com",
+    messagingSenderId: "430212788056",
+    appId: "1:430212788056:web:aac784b141bf4e1904bf2d"
+  }
 };
